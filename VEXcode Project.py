ea335169f841889a9cd600b15349b40fd8e38a89
ZZW <62459108+zwzheng45@users.noreply.github.com>
2022-11-30 10:34:04 +0000

    略微增加了飞轮转速
    从80%&60%增加至90%&70%

--- VEXcode Project.py
+++ VEXcode Project.py
@@ -81,13 +81,13 @@ def user_control(): #user control
             flywheel.stop()
             controller_1.screen.clear_row(3)
         if (controller_1.buttonA.pressing()):
-            flywheel.spin(FORWARD,80,PERCENT)
+            flywheel.spin(FORWARD,90,PERCENT)
             controller_1.screen.clear_row(3)
-            controller_1.screen.print("FlyWheel: 80%        ")
+            controller_1.screen.print("FlyWheel: 90%        ")
         if (controller_1.buttonX.pressing()):
-            flywheel.spin(FORWARD,60,PERCENT)
+            flywheel.spin(FORWARD,70,PERCENT)
             controller_1.screen.clear_row(3)
-            controller_1.screen.print("FlyWheel: 60%        ")
+            controller_1.screen.print("FlyWheel: 70%        ")
 
         #intake
         if (controller_1.buttonR1.pressing()):
